3f8128462618900afd8b67f3d096dfc7d0aaa2ca
sami-rehman <s.rahman@riverprime.com>
2025-08-19 15:58:37 +0400

    final push

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
-  plugins: [import("tailwindcss-animate").then(mod => mod.default ? mod.default : mod)],
-}
